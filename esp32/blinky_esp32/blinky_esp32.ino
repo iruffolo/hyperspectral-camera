2d2e7697cd359067064673f78135502383d655be
Ian Ruffolo <ian.ruffolo-ext@urbanandmainlines.com>
2023-08-13 20:01:49 -0400

    Added logic in blinky for handling different modes from BT

--- esp32/blinky_esp32/blinky_esp32.ino
+++ esp32/blinky_esp32/blinky_esp32.ino
@@ -16,12 +16,22 @@ BluetoothSerial SerialBT;
 // Frequency to cycle LEDs 
 int freq = 500;
 
-char msg[1024];
+bool msg_recv = false;
+
 
 //Violet to Far red
 int const leds[] = {12, 27, 15, 32, 14, SCL, SDA, SCK, A5, 33, RX, TX, 19};
 int const num_leds = 13;
 int const WHITE_LED = SDA;
+
+enum Mode {
+    PASSIVE,
+    RS_CAPTURE,
+    GT_CAPTURE
+};
+
+enum Mode currMode;
+
 
 void setup() {
     Serial.begin(115200); // Start serial connection for debugging
@@ -50,9 +60,25 @@ void loop() {
         read_bluetooth();
     }
 
+    switch (currMode) {
+        // Do nothing, continue to listen on Bluetooth
+        case Mode::PASSIVE:
+            break;
+
+        // Capture rolling shutter images with LEDs cycling
+        case Mode::RS_CAPTURE:
+            
+            break;
+
+        // Capture sequence of GT images with LEDs static 
+        case Mode::GT_CAPTURE:
+            
+            break;
+    }
+
     // cycle_colors_neopixel(freq);
 
-    cycle_led_sequence(0, 1000000);
+    // cycle_led_sequence(0, 100000);
 }
 
 void cycle_led_sequence(int seq_num, int delay_us) {
@@ -94,23 +120,58 @@ void read_bluetooth() {
     int count = 0;
     int new_freq = 0;
 
-    // Recv bluetooth, print to serial
+    char bt_msg[1024];
+    char serial_msg[1024];
+
+    // Recv bluetooth msg until receiving a newline char (indicating end of msg)
     while (SerialBT.available()) {
-        char c = SerialBT.read();
+        const char c = SerialBT.read();
 
-        Serial.write(c);
-
-        if (isdigit(c))
-        {
-            int x = atoi(&c);
-            new_freq = new_freq*10^count + x;
+        if (c == '\n') {
+            msg_recv = true;
+            break;
         }
+
+        strncat(bt_msg, &c, 1);
     }
 
-    freq = new_freq = 0 ? freq : new_freq;
+    if (msg_recv) {
+        Serial.write(bt_msg);
+        Serial.write("\n\r");
 
-    sprintf(msg, "Setting freq: %d", freq);
-    Serial.write(msg);
+        // Protocol is expecting a message of format MODE:VALUE 
+        char *mode;
+        char *value;
 
-    Serial.write("\n\r");
+        // Parse message on ':' delimiter
+        mode = strtok(bt_msg, ":");
+        value = strtok(NULL, ":");
+
+        Serial.write(mode);
+        Serial.write("\n\r");
+
+        if (value != NULL) {
+            int v = atoi(value);
+
+            // Rolling shutter mode
+            if (strcmp(mode, "RS") == 0) {
+                Serial.write("Bloop");
+            }
+            // Ground truth mode
+            else if (strcmp(mode, "GT") == 0) {
+
+            }
+            // Setting param modes
+            // Frequency
+            else if (strcmp(mode, "FREQ") == 0) {
+
+            }
+        }
+
+        // Reset message 
+        memset(bt_msg, 0, 1024);
+        msg_recv = false;
+
+        Serial.write("\n\r"); 
+    }
 }
