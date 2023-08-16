5b3eca9ac32170897246ed338d24a2b3657cdad5
Ian Ruffolo <ian.ruffolo@gmail.com>
2023-08-15 22:28:33 -0400

    removed unused imports

--- app/src/main/java/com/hyperspectral/camera/fragments/BluetoothFragment.kt
+++ app/src/main/java/com/hyperspectral/camera/fragments/BluetoothFragment.kt
@@ -1,8 +1,6 @@
 package com.hyperspectral.camera.fragments
 
-import android.Manifest
 import android.R
-import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
@@ -10,16 +8,13 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.os.Build
 import android.os.Bundle
-import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
