b367bb9188f0ba695819bc2016938486ec020835
Ian Ruffolo <ian.ruffolo-ext@urbanandmainlines.com>
2023-10-23 10:44:01 -0400

    some changes to decode algo

--- scripts/decode.py
+++ scripts/decode.py
@@ -5,7 +5,6 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import rawpy
-from process_raw import DngFile
 
 def readDNG(filename):
     dng_path = f"{os.getcwd()}/data/{filename}"
@@ -13,38 +12,58 @@ def readDNG(filename):
     raw_image = dng.raw_image.astype(int)
     return dng, raw_image
     
-def printImage(image, dpi=200):
-    figure(dpi=dpi)
+# Linearize image
+def linearize(dng, raw):
+    black = dng.black_level_per_channel[0]
+    saturation = dng.white_level
+    raw -= black
+    uint10_max = 2**10 - 1
+    raw = raw * int(uint10_max/(saturation - black))
+    raw = np.clip(raw, 0, uint10_max)
+    return raw
+
+def printImage(image):
     plt.imshow(image, cmap='gray')
     plt.show()
+
+def printStats(image, ax=0):
+    np.set_printoptions(threshold = np.inf)
+
+    avg = np.mean(image, axis=ax)
+    print(f"Min: {np.min(avg)}")
+    print(f"Max: {np.max(avg)}")
+    print(f"Std: {np.std(avg)}")
+    print(f"Avg: {np.mean(avg)}")
+
+
+def denoise(img):
+    dst = cv2.fastNlMeansDenoising(img, None, 5, 7, 21)
+    return dst
+
+
 
 if __name__=="__main__":
 
     fn = "LBL_100_0.dng"
-    dng, img = readDNG(fn)
+    dng, raw = readDNG(fn)
 
-    # raw = dng.raw  # np.uint16
-    # raw_8bit = np.uint8(raw >> (dng.bit-8))
+    printStats(raw)
+    printImage(raw)
 
-    print(img)
-    print(np.mean(img, axis=0))
-
-    avg = np.mean(img, axis=0)
-    print(np.min(avg))
-    print(np.max(avg))
-    print(np.std(avg))
-
-    plt.imshow(img)
+    rgb = dng.postprocess()  # demosaicing by rawpy
+    printStats(rgb, ax=(1,2))
+    plt.imshow(rgb)
     plt.show()
+
+    lin = linearize(dng, raw)
+    print(lin.shape)
+    printStats(lin)
+    plt.imshow(lin)
+    plt.show()
+
+    raw_8bit = np.uint8(lin)
+    dn = denoise(raw_8bit)
+    printImage(dn)
+    printStats(dn)
+
     
-    rgb1 = dng.postprocess()  # demosaicing by rawpy
-    plt.imshow(rgb1)
-    plt.show()
-    print(rgb1.shape)
-
-    avg = np.mean(rgb1, axis=(1,2))
-    print(avg)
-    print(avg.shape)
-    print(np.min(avg))
-    print(np.max(avg))
-    print(np.std(avg))
