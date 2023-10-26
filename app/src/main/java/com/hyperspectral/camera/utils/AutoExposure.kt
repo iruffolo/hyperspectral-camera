d98f9a0e7ec4d69c471735b4903d50cc7a9dd8e8
Ian Ruffolo <ian.ruffolo@gmail.com>
2023-10-26 04:05:07 -0400

    added autofocus limit, and some initial auto exposure code

--- app/src/main/java/com/hyperspectral/camera/utils/AutoExposure.kt
+++ app/src/main/java/com/hyperspectral/camera/utils/AutoExposure.kt
@@ -1,19 +1,37 @@
 package com.hyperspectral.camera.utils
 
 import android.graphics.Bitmap
+import android.graphics.ImageFormat
+import android.graphics.PixelFormat
 import android.media.Image
 import android.media.ImageReader
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
+import android.view.SurfaceHolder
+
+import org.opencv.android.Utils
+import org.opencv.core.Core
+import org.opencv.core.Mat
+
+class AutoExposure(imgWidth: Int, imgHeight: Int) {
+
+    private val tag = "AutoExposure"
+
+    private val imageReaderThread = HandlerThread("aeThread").apply { start() }
+    private val imageReaderHandler = Handler(imageReaderThread.looper)
+
+    val imageReader : ImageReader = ImageReader.newInstance(
+        imgWidth, imgHeight, ImageFormat.JPEG, 1)
 
 
-class AutoExposure {
-
-    val autoExposureListener =
+    private val autoExposureListener =
         ImageReader.OnImageAvailableListener { reader ->
-            Log.d("AutoExposure", "Calculating")
+            Log.d(tag, "Calculating")
             val image = reader.acquireLatestImage()
-//            val imgFormat = image.format
-//            val pixelArray1 = image.planes[0].buffer
+
+//             Log.d(tag, "Format ${image.format}")
+//            val pixelArray = image.planes[0].buffer
 //            val pixelStride = image.planes[0].pixelStride
 //            val rowStride = image.planes[0].rowStride
 //            val rowPadding = rowStride - pixelStride * 640
@@ -22,12 +40,15 @@ class AutoExposure {
 //            bitmap.copyPixelsFromBuffer(pixelArray1)
 //            imageView.setImageBitmap(bitmap)
 
+//            Log.d(tag, "Format ${image.format}")
+//            Log.d(tag, "pixelStride ${pixelStride}")
+//            Log.d(tag, "rowStride ${rowStride}")
 
             image.close()
         }
+    init {
+        Log.d(tag, "Initializing AE")
 
-    fun calculateISO(img: Image)
-    {
-
+        imageReader.setOnImageAvailableListener(autoExposureListener, imageReaderHandler)
     }
 }
