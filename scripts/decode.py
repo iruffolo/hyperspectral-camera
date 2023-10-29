ec28b92fd02edd897458d9509189765d1adbec7e
Ian Ruffolo <ian.ruffolo@gmail.com>
2023-10-29 00:06:50 -0400

    Row labelling algo done, some updates for black band searching

--- scripts/decode.py
+++ scripts/decode.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib
 from rawpy import FBDDNoiseReductionMode
 from skimage import color
-# from scipy import ndimage
+from scipy import ndimage
 from label_rows import calc_labels
 matplotlib.use('TkAgg')
 
@@ -54,27 +54,13 @@ def denoise(img):
     return dst
 
 
-def rollingAvg(img, w=4, fw=1):
+def rollingAvg(img, w=2, fw=1):
 
-    # roll = ndimage.uniform_filter(img, size=(fw, img.shape[1]), mode='wrap')
+    img = ndimage.uniform_filter(img, size=(fw, img.shape[1]), mode='wrap')
 
-    roll = np.zeros_like(img)
+    stacking = np.array([np.roll(img, x, axis=0) for x in range(-w, w+1)])
+    roll = np.mean(stacking, axis=0)
 
-    for i in range(0, roll.shape[0]):
-        x = np.zeros_like(img[0, :])
-
-        for j in range(-w, w+1):
-            idx = i+j
-            if (idx < 0 or idx > roll.shape[0]-1):
-                roll[i, :] = np.ones_like(img[0, :])
-                break
-            else:
-                x = np.stack((x, img[idx, :]), axis=0)
-                x = np.sum(x, axis=0)
-            roll[i, :] = x / ((w*2) + 1)
-
-    # roll = ndimage.uniform_filter(roll,
-    #                             size=(fw, roll.shape[1]), mode='wrap')
     return roll
 
 
@@ -82,11 +68,9 @@ if __name__ == "__main__":
 
     # np.set_printoptions(threshold = np.inf)
     dir = f"{os.getcwd()}/data_cc/"
-
     files = os.listdir(dir)
 
     params = dict()
-
     map = ['scenario', 'timestamp', 'mode', 'frame_num', 'ton',
            'toff', 'black_mul', 'mux', 'exposure_time', 'iso']
 
@@ -99,82 +83,128 @@ if __name__ == "__main__":
     sorted_files = sorted(params,
                           key=lambda x: params[x]['timestamp'])
 
-    fn = sorted_files[8]
+    print(sorted_files)
+    fn = sorted_files[1]
     dng, raw = readDNG(dir + fn)
 
     # lin = linearize(dng, raw)
 
     # demosaicing by rawpy
-    rgb = dng.postprocess(gamma=(2.22, 4.5),
+    rgb = dng.postprocess(gamma=(100, 4.5),
                           no_auto_bright=False,
                           fbdd_noise_reduction=FBDDNoiseReductionMode.Full,
-                          use_camera_wb=True,
+                          use_camera_wb=False,
+                          use_auto_wb=True,
                           user_black=dng.black_level_per_channel[0],
+                          dcb_enhance=True,
+                          dcb_iterations=5,
                           output_bps=8)
 
     fig = plt.figure(figsize=(25, 5))
     fig.suptitle(f"{fn}", fontsize=25)
 
+    titles = ("Raw", "RGB", "Gray", "UF + Moving Avg", "Mask")
     row = 1
-    col = 7
-    titles = ("Red", "Green", "Blue", "RGB", "Gray",
-              "Uniform Filter + Moving Avg", "Mask (Black)")
+    col = len(titles)
 
-    for i in range(0, 3):
-        fig.add_subplot(row, col, i+1)
-        plt.title(titles[i])
-        plt.imshow(rgb[:, :, i])
+    # RAW
+    fig.add_subplot(row, col, 1)
+    plt.title(titles[0])
+    lin = linearize(dng, raw)**(1/100)
+    lin_a = rollingAvg(lin, w=1, fw=15)
 
-    fig.add_subplot(row, col, 4)
-    plt.title(titles[3])
-    plt.imshow(rgb)
+    source = lin_a
+    avg_row = np.mean(source, axis=1)
+    std_row = np.std(source, axis=1)
+    stacking = np.array([np.roll(avg_row, x) for x in range(-2, 3)])
+    avg_cols = np.mean(stacking, axis=0)
+    std_cols = np.std(stacking, axis=0)
+
+    plt.imshow(lin_a.T, cmap='gray')
+    plt.plot(-1*(avg_row+1)**11, alpha=0.4)
+
+    fig.add_subplot(row, col, 2)
+    plt.title(titles[1])
+    plt.imshow(np.transpose(rgb, axes=(1, 0, 2)))
+    plt.plot(-1*(avg_row+1)**11, alpha=0.4)
 
     gray = color.rgb2gray(rgb)
-    fig.add_subplot(row, col, 5)
-    plt.title(titles[4])
-    plt.imshow(gray, cmap='gray')
+    fig.add_subplot(row, col, 3)
+    plt.title(titles[2])
+    plt.imshow(gray.T, cmap='gray')
+    plt.plot(-1*(avg_row+1)**11, alpha=0.4)
 
-    # # Cut edges off rows
-    # cols = rgb.shape[1]
-    # uslice = int(cols - cols/4)
-    # lslice = int(cols - cols*3/4)
-    # a = rollingAvg(gray[:,lslice:uslice])
     a = rollingAvg(gray)
-
-    fig.add_subplot(row, col, 6)
-    plt.title(titles[5])
-    plt.imshow(a, cmap='gray')
-
-    avg_gray = np.mean(a, axis=1)
-    std_gray = np.std(a, axis=1)
-    # avg_channel = np.mean(gray, axis=1)
-    # avg = np.mean(avg_channel, axis=1)
-    # sum = np.sum(avg_channel, axis=1)
-    # diff = np.max(avg_channel, axis=1) - np.min(avg_channel, axis=1)
-    # std = np.mean(np.std(a, axis=1), axis=1)
+    fig.add_subplot(row, col, 4)
+    plt.title(titles[3])
+    plt.imshow(a.T, cmap='gray')
+    plt.plot(-1*(avg_row+1)**11, alpha=0.4)
 
     # Array for labelling rows
     mask_black = np.zeros(gray.shape[0])
 
     # mask = np.where((avg < 50) & (sum < 150) & (diff < 35))
-    mask = np.where((avg_gray < 0.20) & (std_gray < 0.1))
-    np.put(mask_black, mask, 1)
+    search_grid = [x/1000 for x in range(230, 300, 1)]
+    print(search_grid)
+
+    # for threshold in search_grid:
+    #     cond = (avg_row < threshold) & (avg_cols < threshold) & (std_cols < 0.01)
+    #     masks = consecutive(np.where(cond)[0])
+
+    # black_rows = np.where(np.roll(mask, 1) - mask)[0]
+    # print(black_rows)
+    # if (black_rows > 1):
+    #     mid = [int((black_rows[i] + black_rows[i+1])/2)
+    #            for i in range(0, black_rows.size, 2)]
+    #     print(mid)
+
+    def consecutive(x, stepsize=5):
+        return np.split(x, np.where(np.diff(x) > stepsize)[0]+1)
+
+    r_threshold = 0.58
+    cond = (avg_row < r_threshold) & (std_cols < 0.005)
+    masks = np.where(cond)[0]
+    mask_groups = consecutive(masks)
+    print(masks)
+    print(mask_groups)
+    np.put(mask_black, masks, 1)
 
     # Debug
     # for i,x in enumerate(sum):
     #     print(i, avg_channel[i], avg[i], x, diff[i], std[i])
-    for i, x in enumerate(avg_gray):
-        print(i, x, std_gray[i])
+    for i, x in enumerate(avg_row):
+        print(i, x, avg_cols[i], std_row[i], std_cols[i])
 
     mask_img_black = np.repeat(mask_black[:, np.newaxis], raw.shape[1], axis=1)
-    fig.add_subplot(row, col, 7)
-    plt.title(titles[6])
-    plt.imshow(mask_img_black, cmap='gray')
-    plt.tight_layout()
-    plt.show()
+    fig.add_subplot(row, col, 5)
+    plt.title(titles[4])
+    plt.imshow(mask_img_black.T, cmap='gray')
+    plt.plot(-1*(avg_row+1)**11, alpha=0.4)
 
-    row_labels = calc_labels(mask_black,
+    mask_groups = consecutive(masks)
+    print(mask_groups)
+    # Drop smallest groups
+    if (len(mask_groups) > 3):
+        mask_groups.sort(key=lambda x: len(x), reverse=True)
+        mask_groups = mask_groups[:3]
+    # Resort in order
+    mask_groups.sort(key=lambda x: sum(x))
+
+    # Find middle row in group
+    mids = np.array([int((np.max(x)-np.min(x))/2) + np.min(x)
+                     for x in mask_groups])
+
+    # black_rows = [np.array([x - 6, x + 6]) for x in mids]
+    # print(black_rows)
+
+    row_labels = calc_labels(mask_black.shape[0],
+                             mids,
                              float(params[fn]['ton']),
                              float(params[fn]['toff']),
                              float(params[fn]['black_mul']))
     print(row_labels)
+    print(row_labels.shape)
+
+    plt.tight_layout()
+    # plt.show()
+
--- scripts/label_rows.py
+++ scripts/label_rows.py
@@ -2,19 +2,21 @@
 
 import numpy as np
 
-color_map = [
-   "VIOLET",        # 0 
-   "ROYAL BLUE",
-   "BLUE",
-   "CYAN",
-   "GREEN",
-   "LIME",          # 5
-   "AMBER",
-   "RED ORANGE",
-   "RED",
-   "DEEP RED",
-   "FAR RED",       # 5
-   "WHITE"]
+LED_SEQUENCE = [
+    "VIOLET",        # 0
+    "ROYAL BLUE",
+    "BLUE",
+    "CYAN",
+    "GREEN",
+    "LIME",          # 5
+    "AMBER",
+    "RED ORANGE",
+    "RED",
+    "DEEP RED",
+    "FAR RED",       # 10
+    "WHITE",
+    "BLACK"
+]
 
 
 def calc_num_rows(ton, toff, size):
@@ -24,38 +26,87 @@ def calc_num_rows(ton, toff, size):
 
     return (int)(size/denom)
 
-def calc_labels(mask, ton, toff, black_mul, fname='seq1.npy'):
-    seq = np.load(fname)
 
-    n = calc_num_rows(ton, toff, seq.shape[1])
+def consecutive(x, stepsize=3):
+    return np.split(x, np.where(np.diff(x) > stepsize)[0]+1)
+
+
+def one_hot_encode(x):
+    x = np.array(x).astype(int)
+    one_hot = np.zeros((x.size, x.max() + 1))
+    one_hot[np.arange(x.size), x] = 1
+
+    # Lazy zero blacks (black is index 12)
+    one_hot = one_hot.T[1:].T
+
+    return one_hot
+
+
+def make_color_map(seq, n, v=False):
+
     col_seq = np.ravel(seq[0][:n])
 
-    colors = [color_map[x] for x in col_seq]
-    colors.insert(0, "BLUE") 
-    colors.append("RED") 
+    col_seq = np.insert(col_seq, 0, LED_SEQUENCE.index("BLUE"))
+    col_seq = np.append(col_seq, LED_SEQUENCE.index("RED"))
 
-    # Find midpoint of black rows
-    black_rows = np.where(np.roll(mask,1) != mask)[0]
-    mid = [int((black_rows[i] + black_rows[i+1])/2) for i in range(0, black_rows.size, 2)]
-    print(black_rows)
-    print(mid)
-    print(np.diff(mid))
-    print(np.diff(black_rows))
+    col_seq = np.insert(col_seq, 0, LED_SEQUENCE.index("BLACK"))
+    col_seq = np.append(col_seq, LED_SEQUENCE.index("BLACK"))
 
-    # print(black_rows[np.where(np.diff(black_rows) < 100)])
-    seq_len = np.diff(mid)
+    if v:
+        print("Color sequence:")
+        print(col_seq)
+        print(f"Length: {len(col_seq)}")
 
-    # num_rows_per_led
-    print(seq_len)
-
-    return 0
+    return col_seq
 
 
-if __name__=="__main__":
+def calc_labels(nrows, black_rows, ton, toff, black_mul,
+                resolution=10, fname='seq1.npy'):
+
+    labels = np.ones(nrows*resolution) * LED_SEQUENCE.index("BLACK")
+
+    # Generated color sequence of LEDs
+    seq = np.load(fname)
+    n_og_seq = calc_num_rows(ton, toff, seq.shape[1])
+    color_map = make_color_map(seq, n_og_seq, v=True)
+    n = len(color_map)
+
+    black_rows = black_rows*resolution
+    print(f"Black rows: {black_rows}")
+
+    row_span = np.diff(black_rows)[0]
+    print(f"Number of rows between black: {row_span}")
+    repeat_leds = int(row_span / n)
+    print(f"Repeat LEDs: {repeat_leds}x")
+
+    color_map = np.repeat(color_map, repeat_leds)
+
+    black_pad = int((row_span % repeat_leds)/2)
+
+    # 0 -> first black row
+    target_row = black_rows[0] - black_pad
+    labels[0:target_row] = np.array(color_map[-target_row:])
+    # print(labels[target_row-900:target_row+50])
+
+    # Inbetween all black rows
+    for i in range(black_rows.shape[0]-1):
+        target_row_start = black_rows[i] + black_pad
+        target_row_end = black_rows[i+1] - black_pad
+        labels[target_row_start:target_row_end] = np.array(color_map)
+
+    # last black row -> end
+    target_row = black_rows[-1] + black_pad
+    labels[target_row:] = np.array(color_map[:len(labels)-target_row])
+    # print(labels[target_row:target_row+900])
+
+    return one_hot_encode(labels)
+
+
+if __name__ == "__main__":
 
     seq = np.load('seq1.npy')
-    print(seq)
-    print(seq.shape)
+    # print(seq)
+    # print(seq.shape)
+    # print(calc_num_rows(15, 0, seq.shape[1]))
 
-    print(calc_num_rows(15, 0, seq.shape[1]))
-
+    print(one_hot_encode(np.array([12.0, 2, 10, 1, 6, 3, 7, 4, 3, 1, 0])))
