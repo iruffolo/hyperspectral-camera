fce8f6be9a2a17d03f8a57ac37cf3972732c67e6
Ian Ruffolo <ian.ruffolo-ext@urbanandmainlines.com>
2023-10-31 15:39:52 -0400

    added script for matlab parsing spectral data

---
+++ scripts/matlab/read_mat.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import scipy.io
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+if __name__ == "__main__":
+
+    data_dir = "/home/ian/dev/KonicaMinolta-CS2000-MATLAB/data"
+
+    mat = scipy.io.loadmat(f"{data_dir}/measure01_spectral.mat")
+
+    sd = mat["spectralData"]
+    print(sd)
+
+    print(sd.shape)
+    lmin = 380
+    lmax = 780
+    step = 1
+
+    x = list(range(lmin, lmax+1, step))
+
+    plt.plot(x, sd[0])
+    plt.gca().set_ylim([np.min(sd), np.max(sd)])
+    plt.gca().set_xlim([lmin, lmax])
+    plt.show()
